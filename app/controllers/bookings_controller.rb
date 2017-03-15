1b9822f50c77ed7be3fbbc52b20f099be00d183e
hansheng quek <hsquek06@gmail.com>
2017-03-15 18:51:48 +0800

    Booking form: Calender months now render using AJAX. Time validation for booking model complete.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -32,6 +32,7 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking.destroy
   end
 
   def show
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,44 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :facility_type
   before_save :set_start_time
+  before_save :testing
+
+  def testing
+    p "-------------------------"
+    p facility_type_id.to_i.class
+    p facility_type_id.class
+    p "-------------------------"
+  end
+
+  # fac_open = FacilityType.find("#{facility_type_id}".to_i).opens_at
+  # fac_close = FacilityType.find("#{facility_type_id}".to_i).closes_at
+  # ses_len = FacilityType.find("#{facility_type_id}".to_i).session_length
+
+  # validates :start_at, inclusion: { in: fac_open..(fac_close - ses_len*3600)}, if: :val_start_time?
+
+
+  validates :booking_date, presence: true
+  validates :user_id, presence: true
+  validate :val_start_time
+
+
+  def val_start_time
+    if facility_type_id != 4
+      fac_open = FacilityType.find(facility_type_id).opens_at
+      fac_close = FacilityType.find(facility_type_id).closes_at
+      ses_len = FacilityType.find(facility_type_id).session_length
+      if !(start_at.between?(fac_open,(fac_close - ses_len*3600)))
+        p "-------------------------"
+        p 'this should fail'
+        p "-------------------------"
+        errors.add(:start_at, "needs to be between #{fac_open} and #{fac_close}")
+      else
+        p "-------------------------"
+        p 'this didnt fail'
+        p "-------------------------"
+      end
+    end
+  end
 
   def set_start_time
     self.start_time = "#{booking_date} #{start_at}"
