d015f9c4e432ceac9303de28547d95f7a5605f66
hansheng quek <hsquek06@gmail.com>
2017-03-16 01:25:33 +0800

    Bookings: Calendar and form views properly rendered, booking model validation completed

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,25 +1,43 @@
 // event delegation (.live)
 //
-// $(document).on('click', 'td', function (e) {
-//   if (!$(e.currentTarget).text().match(/No more available slots/)) {
-//     $('.day').css({'background-color': ''})
-//     var bookDate = $(e.currentTarget)
-//     .clone()    // clone the element
-//     .children() // select all the children
-//     .remove()   // remove all the children
-//     .end()  // again go back to selected element
-//     .text()
-//     .replace(/[\n\s]/g, '')
+
+// $(document).on('ajaxSuccess', '#calendar', function (e) {
+//   console.log($('td'));
+//   $('td').forEach(function(item) {
+//     item.text().replace(/[\n\s(\d*-\d\d-)]/g, '')
 //
-//     $(e.currentTarget).css('background-color', 'yellow')
-//
-//     $('.booking_date_input').val(bookDate)
-//   }
+//   })
+//   console.log($('td').text());
 //
 // })
+
+$(document).on('click', 'td', function (e) {
+  if (!$(e.currentTarget).text().match(/No more available slots/) && $(e.currentTarget).text().match(/Book/)) {
+    $('.day').css({'background-color': ''})
+    // var bookDate = $(e.currentTarget)
+    // .clone()    // clone the element
+    // .children() // select all the children
+    // .remove()   // remove all the children
+    // .end()  // again go back to selected element
+    // .text()
+    // .replace(/[\n\s]/g, '')
+
+    $(e.currentTarget).css('background-color', 'yellow')
+    // $(e.currentTarget).attr('data-remote', true)
+    // window.location = $(e.currentTarget).find("a").attr("href");
+    // return false;
+
+    // $('.booking_date_input').val(bookDate)
+  }
+
+})
 
 $(document).on('mouseover', 'a', function (e) {
   if ($(e.currentTarget).text().match(/Next/) || $(e.currentTarget).text().match(/Previous/)) {
     $(e.currentTarget).attr('data-remote', true)
   }
+})
+
+$(document).on('click', '.fac-types', function (e) {
+  $('#booking-form').empty()
 })
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -33,7 +33,9 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
     @booking.destroy
+    redirect_to root_path
   end
 
   def mybookings
