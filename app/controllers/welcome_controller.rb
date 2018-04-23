801f005ed246c01591d0a2a4ac089dacb7139c0a
Quek Han Sheng <hsquek06@gmail.com>
2018-04-23 14:51:29 +0800

    disable scraper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-    url = "http://bleacherreport.com/tennis"
-    url2 = "http://www.bbc.com/sport/swimming"
-    doc2 = Nokogiri::HTML(open(url2))
-    doc = Nokogiri::HTML(open(url))
-    @results = doc.css('.articleContent')
-    @swimming_results = doc2.css('.lakeside__body')
-
-    url3 = "https://www.eventbrite.sg/d/marine-parade/events/?crt=regular&sort=date"
-    doc_events = Nokogiri::HTML(open(url3))
-    @events = doc_events.css('.list-card-v2')
+    # url = "http://bleacherreport.com/tennis"
+    # url2 = "http://www.bbc.com/sport/swimming"
+    # doc2 = Nokogiri::HTML(open(url2))
+    # doc = Nokogiri::HTML(open(url))
+    # @results = doc.css('.articleContent')
+    # @swimming_results = doc2.css('.lakeside__body')
+    #
+    # url3 = "https://www.eventbrite.sg/d/marine-parade/events/?crt=regular&sort=date"
+    # doc_events = Nokogiri::HTML(open(url3))
+    # @events = doc_events.css('.list-card-v2')
 
     # url = "http://feeds.bbci.co.uk/sport/tennis/rss.xml?edition=int#"
     # @feed = Feedjira::Feed.fetch_and_parse(url)
